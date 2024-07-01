00122746e05e30204cbe4ada44bb93b56b5d7264
Usama-Khan99 <65035298+Usama-Khan99@users.noreply.github.com>
2024-07-02 00:03:42 +0500

    fresh and stale object

--- main.ts
+++ main.ts
@@ -12,3 +12,27 @@
 // These two interfaces are completely 
 // transferable in a structural type system
 
+
+
+// Fresh/ Stale/ Dynamic Objects 
+
+let myType = {name: 'usama', id:1}
+
+myType = {id:2, name: 'usama'}
+
+// Fresh object in structural typing 
+
+// fresh object (is m same structure banana parhta h jou variable banaty wqt rakhty h)
+// same stucture follow karna parhta h jou assign hota h
+myType = {id:2, name: 'usama', age:25}
+
+// Stale object in structral typing
+
+let testType = {id:2, name: 'usama', age:25}
+
+myType = testType;
+
+
+// Must practice fresh and stale object 
+
+
