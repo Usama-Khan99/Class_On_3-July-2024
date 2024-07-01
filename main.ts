7e3c9f353b4ad786bfcbb818e1c3f05822383ada
Usama-Khan99 <65035298+Usama-Khan99@users.noreply.github.com>
2024-07-01 23:55:31 +0500

    Revision

---
+++ main.ts
@@ -0,0 +1,14 @@
+// Difference between enum and type literals
+// Difference between enum and const enum
+
+// Exam Material
+
+// Github repo (10 steps till tuples)
+// Slides must watch
+// Raman coding night must watch
+// 45 Assignments
+
+
+// These two interfaces are completely 
+// transferable in a structural type system
+
