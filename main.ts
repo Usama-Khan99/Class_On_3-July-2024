4ab8229e4e144a012e2f3af1687dd7aa50f0d6c5
Usama-Khan99 <65035298+Usama-Khan99@users.noreply.github.com>
2024-07-03 20:15:51 +0500

    Index signature

--- main.ts
+++ main.ts
@@ -35,4 +35,25 @@ myType = testType;
 
 // Must practice fresh and stale object 
 
+// Index signature
 
+// jbb ye naa pta ho k hamary form m kitni keys hongi or kitni values tou
+// index signature use karygy
+
+type dynamicForm = {
+    [key:string]: string;
+
+}
+
+let feedBackForm : dynamicForm = {
+name:'Usama Khan',
+email: 'usama@gmail.com',
+feedback: "I'm happy with service.",
+};
+
+let studentInfo: dynamicForm ={
+    name:'Usama khan',
+    email:'usama@gmail.com',
+    nic:'1234918356826',
+    education:'Bsc',
+};
